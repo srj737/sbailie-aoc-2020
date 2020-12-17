ee1c35ae07c749fa779bb5b6b5ac26215969d7b4
Steven Bailie <srjbailie@gmail.com>
2020-12-17 17:29:13 +0000

    Day 17 (Part 2): Conway's Automota in 4D (Unfortunaly doesn't scale to n dimensions because I spent enough time already...)

--- day17.py
+++ day17.py
@@ -10,68 +10,90 @@ ACTIVE = '#'
 INACTIVE = '.'
 
 
-def get_3d_neighbour_indexes(i, j, k):
-    return [
-        (i - 1, j - 1, k - 1),
-        (i - 1, j - 1, k + 0),
-        (i - 1, j - 1, k + 1),
-        (i - 1, j + 0, k - 1),
-        (i - 1, j + 0, k + 0),
-        (i - 1, j + 0, k + 1),
-        (i - 1, j + 1, k - 1),
-        (i - 1, j + 1, k + 0),
-        (i - 1, j + 1, k + 1),
-        (i + 0, j - 1, k - 1),
-        (i + 0, j - 1, k + 0),
-        (i + 0, j - 1, k + 1),
-        (i + 0, j + 0, k - 1),
-        # (i + 0, j + 0, k + 0),
-        (i + 0, j + 0, k + 1),
-        (i + 0, j + 1, k - 1),
-        (i + 0, j + 1, k + 0),
-        (i + 0, j + 1, k + 1),
-        (i + 1, j - 1, k - 1),
-        (i + 1, j - 1, k + 0),
-        (i + 1, j - 1, k + 1),
-        (i + 1, j + 0, k - 1),
-        (i + 1, j + 0, k + 0),
-        (i + 1, j + 0, k + 1),
-        (i + 1, j + 1, k - 1),
-        (i + 1, j + 1, k + 0),
-        (i + 1, j + 1, k + 1)
-    ]
+def calc_neighbour_indexes(dimensions, n, i, j, k, m):
+    # 3D ==========
+    if dimensions == 3:
+        indexes = set([(x, y, z)
+                       for x in range(i - 1, i + 2)
+                       for y in range(j - 1, j + 2)
+                       for z in range(k - 1, k + 2)
+                       if (0 <= x < n and 0 <= y < n and 0 <= z < n)])
+        indexes.remove((i, j, k))
+    # 4D ==========
+    elif dimensions == 4:
+        indexes = set([(x, y, z, w)
+                       for x in range(i - 1, i + 2)
+                       for y in range(j - 1, j + 2)
+                       for z in range(k - 1, k + 2)
+                       for w in range(m - 1, m + 2)
+                       if (0 <= x < n and 0 <= y < n and 0 <= z < n and 0 <= w < n)])
+        indexes.remove((i, j, k, m))
+    # Common =====
+    return indexes
 
 
 class ConwayCubes(object):
 
     def __init__(self, starting_2d_array, size, dimensions):
         self.size = size
-        matrix = [[[INACTIVE for k in range(size)] for j in range(size)] for i in range(size)]
-        next_matrix = [[[INACTIVE for k in range(size)] for j in range(size)] for i in range(size)]
+        self.dimensions = dimensions
         offset = round(size / 2) - 1
-        for row_idx, row in enumerate(starting_2d_array):
-            for col_idx, value in enumerate(row):
-                matrix[offset][row_idx + offset][col_idx + offset] = value
+        # 3D ==========
+        if self.dimensions == 3:
+            matrix = [[[INACTIVE for k in range(size)] for j in range(size)] for i in range(size)]
+            for row_idx, row in enumerate(starting_2d_array):
+                for col_idx, value in enumerate(row):
+                    matrix[offset][row_idx + offset][col_idx + offset] = value
+        # 4D ==========
+        elif self.dimensions == 4:
+            matrix = [[[[INACTIVE for m in range(size)] for k in range(size)] for j in range(size)] for i in
+                      range(size)]
+            for row_idx, row in enumerate(starting_2d_array):
+                for col_idx, value in enumerate(row):
+                    matrix[offset][offset][row_idx + offset][col_idx + offset] = value
+        # Common =====
         self.matrix = matrix
-        self.next_matrix = next_matrix
+        self.next_matrix = matrix
+        self.energy_count = self.count_active_cubes()
 
     def cycle(self):
-        next_matrix = [[[INACTIVE for k in range(self.size)] for j in range(self.size)] for i in range(self.size)]
-        for i_idx, plane in enumerate(self.matrix):
-            for j_idx, row in enumerate(plane):
-                for k_idx, value in enumerate(row):
-                    next_matrix[i_idx][j_idx][k_idx] = self.calc_new_state(i_idx, j_idx, k_idx, value)
+        # 3D ==========
+        if self.dimensions == 3:
+            next_matrix = [[[INACTIVE for k in range(self.size)] for j in range(self.size)] for i in range(self.size)]
+            for i_idx, plane in enumerate(self.matrix):
+                for j_idx, row in enumerate(plane):
+                    for k_idx, value in enumerate(row):
+                        next_matrix[i_idx][j_idx][k_idx] = self.calc_new_state(value, i_idx, j_idx, k_idx)
+        # 4D ==========
+        elif self.dimensions == 4:
+            next_matrix = [[[[INACTIVE for m in range(self.size)] for k in range(self.size)] for j in range(self.size)]
+                           for i in range(self.size)]
+            for i_idx, space in enumerate(self.matrix):
+                for j_idx, plane in enumerate(space):
+                    for k_idx, row in enumerate(plane):
+                        for m_idx, value in enumerate(row):
+                            next_matrix[i_idx][j_idx][k_idx][m_idx] = self.calc_new_state(value, i_idx, j_idx, k_idx,
+                                                                                          m_idx)
+        # Common =====
         self.matrix = next_matrix
         self.energy_count = self.count_active_cubes()
 
-    def calc_new_state(self, i, j, k, value):
-        neighbour_indexes = get_3d_neighbour_indexes(i, j, k)
+    def calc_new_state(self, value, i, j, k, m=0):
         active_neighbour_count = 0
+        neighbour_indexes = self.calc_neighbour_indexes(i, j, k, m)
         for index in neighbour_indexes:
-            if all(0 <= x < self.size for x in index):
+            # 3D ==========
+            if self.dimensions == 3:
                 check = self.matrix[index[0]][index[1]][index[2]]
-                if check == ACTIVE:
-                    active_neighbour_count += 1
+            # 4D ==========
+            elif self.dimensions == 4:
+                check = self.matrix[index[0]][index[1]][index[2]][index[3]]
+            # Common =====
+            if check == ACTIVE:
+                active_neighbour_count += 1
+            # As NOTHING will ever change if there are >3 active neighbours, we can break at 4 (Minor Performance)
+            if active_neighbour_count > 3:
+                break
         # If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active.
         # Otherwise, the cube becomes inactive.
         if value == ACTIVE:
@@ -87,18 +109,51 @@ class ConwayCubes(object):
             else:
                 return INACTIVE
 
+    def calc_neighbour_indexes(self, i, j, k, m):
+        n = self.size
+        # 3D ==========
+        if self.dimensions == 3:
+            indexes = set([(x, y, z)
+                           for x in range(i - 1, i + 2)
+                           for y in range(j - 1, j + 2)
+                           for z in range(k - 1, k + 2)
+                           if (0 <= x < n and 0 <= y < n and 0 <= z < n)])
+            indexes.remove((i, j, k))
+        # 4D ==========
+        elif self.dimensions == 4:
+            indexes = set([(x, y, z, w)
+                           for x in range(i - 1, i + 2)
+                           for y in range(j - 1, j + 2)
+                           for z in range(k - 1, k + 2)
+                           for w in range(m - 1, m + 2)
+                           if (0 <= x < n and 0 <= y < n and 0 <= z < n and 0 <= w < n)])
+            indexes.remove((i, j, k, m))
+        # Common =====
+        return indexes
+
     def count_active_cubes(self):
         active_cubes = 0
-        for i_idx, plane in enumerate(self.matrix):
-            for j_idx, row in enumerate(plane):
-                for k_idx, value in enumerate(row):
-                    if self.matrix[i_idx][j_idx][k_idx] == ACTIVE:
-                        active_cubes += 1
+        # 3D ==========
+        if self.dimensions == 3:
+            for i_idx, plane in enumerate(self.matrix):
+                for j_idx, row in enumerate(plane):
+                    for k_idx, value in enumerate(row):
+                        if self.matrix[i_idx][j_idx][k_idx] == ACTIVE:
+                            active_cubes += 1
+        # 4D ==========
+        elif self.dimensions == 4:
+            for i_idx, space in enumerate(self.matrix):
+                for j_idx, plane in enumerate(space):
+                    for k_idx, row in enumerate(plane):
+                        for m_idx, value in enumerate(row):
+                            if self.matrix[i_idx][j_idx][k_idx][m_idx] == ACTIVE:
+                                active_cubes += 1
+        # Common =====
         return active_cubes
 
 
-def day17_part1(tickets_input, size):
-    pocket_dimension = ConwayCubes(tickets_input, size, 3)
+def day17(tickets_input, size, dimensions):
+    pocket_dimension = ConwayCubes(tickets_input, size, dimensions)
     target = 6
     for n in range(target):
         pocket_dimension.cycle()
@@ -114,28 +169,28 @@ main_input = ingest_file.char_matrix('input/day17.txt')
 
 # Note: Initially I just guessed the size the matrix had to be, and if the final output didn't change when increasing
 # the size by +1 then I knew it was sufficient. I then attempted smaller and smaller sizes while getting the same
-# answer to get the best perfromance.
+# answer to get the best performance.
 
 # Test Input - Part 1
-if day17_part1(test_input, 16) != 112:
+if day17(test_input, 16, 3) != 112:
     raise Exception("FAIL: Part 1 Test FAILED!")
 else:
     logging.info("PASS: Part 1 Test PASSED")
 
 # Real Input - Part 1
-if day17_part1(main_input, 21) != 310:
+if day17(main_input, 21, 3) != 310:
     raise Exception("FAIL: Part 1 FAILED!")
 else:
     logging.info("PASS: Part 1 PASSED")
 
 # Test Input - Part 2
-# if day17_part2(test_input):
-#    raise Exception("FAIL: Part 2 Test FAILED!")
-# else:
-#    logging.info("PASS: Part 2 Test PASSED")
+if day17(test_input, 16, 4) != 848:
+    raise Exception("FAIL: Part 2 Test FAILED!")
+else:
+    logging.info("PASS: Part 2 Test PASSED")
 
 # Real Input - Part 2
-# if day17_part2(main_input) != 491924517533:
-#    raise Exception("FAIL: Part 2 FAILED!")
-# else:
-#    logging.info("PASS: Part 2 PASSED")
+if day17(main_input, 24, 4) != 2056:
+    raise Exception("FAIL: Part 2 FAILED!")
+else:
+    logging.info("PASS: Part 2 PASSED")
