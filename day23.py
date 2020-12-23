11c22f1af1e2e51944e5c08fff02b3b3356b46cd
Steven Bailie <srjbailie@gmail.com>
2020-12-23 18:33:41 +0000

    Day 23: Part 2 (Had to almost entirely rewrite Part 1 with dictionary rather than lists)

--- day23.py
+++ day23.py
@@ -9,84 +9,87 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,
 
 class Cups:
 
-    def __init__(self, data_input):
-        self.cups = data_input.copy()
+    def __init__(self, data_input, total):
+        self.cups = {}
+        for idx, cup in enumerate(data_input):
+            self.cups[cup] = data_input[(idx + 1) % len(data_input)]
+        if len(self.cups) < total:
+            next_key = data_input[-1]
+            next_value = max(self.cups) + 1
+            while next_value <= total:
+                self.cups[next_key] = next_value
+                next_key, next_value = next_value, next_value + 1
+            self.cups[next_value - 1] = data_input[0]
         self.round = 0
-        self.current_label = self.cups[0]
+        self.current = data_input[0]
+        self.min = min(self.cups)
+        self.max = max(self.cups)
         return
 
     def run_game(self, game_length):
         while self.round < game_length:
             self.run_round()
+            if self.round % 1000000 == 0:
+                logging.info('PENDING: Completed round %d/%d', self.round, game_length)
         return
 
     def run_round(self):
         self.round += 1
-        current_idx = self.cups.index(self.current_label)
-
         # Picks up next three cups
-        pick_up = self.pick_up_cups(current_idx, 3)
-
+        pick_up = [self.cups[self.current],
+                   self.cups[self.cups[self.current]],
+                   self.cups[self.cups[self.cups[self.current]]]]
+        self.cups[self.current] = self.cups[self.cups[self.cups[self.cups[self.current]]]]
         # Selects destination cup
-        destination_idx = self.find_destination_index(self.current_label)
-
-        # Places picked up cups after destination
-        for cup in pick_up:
-            destination_idx += 1
-            self.cups.insert(destination_idx, cup)
-
+        destination = self.find_destination_index(self.current, pick_up)
+        # Places the picked up cups after destination
+        self.cups[pick_up[2]] = self.cups[destination]
+        self.cups[pick_up[1]] = pick_up[2]
+        self.cups[pick_up[0]] = pick_up[1]
+        self.cups[destination] = pick_up[0]
         # Iterate current cup (directly clockwise of current cup)
-        self.current_label = self.cups[(self.cups.index(self.current_label) + 1) % len(self.cups)]
+        self.current = self.cups[self.current]
         return
 
-    def find_destination_index(self, label):
-        destination_index = 'NotFound'
-        while destination_index == 'NotFound':
-            destination_label = label - 1
-            if destination_label in self.cups:
-                destination_index = self.cups.index(destination_label)
-            label -= 1  # Keep searching down
-            if label < min(self.cups):
-                label = max(self.cups) + 1
-        return destination_index
-
-    def pick_up_cups(self, current_idx, number_to_pick):
-        pu_start = current_idx + 1
-        pu_end = current_idx + 1 + number_to_pick
-        cup_length = len(self.cups)
-        if pu_start < cup_length and pu_end < cup_length:
-            pick_up = self.cups[pu_start: pu_end]
-            del (self.cups[pu_start: pu_end])
-        elif pu_start < cup_length and pu_end >= cup_length:
-            pu_end = pu_end % cup_length
-            pick_up = self.cups[pu_start:] + self.cups[:pu_end]
-            del (self.cups[pu_start:])
-            del (self.cups[:pu_end])
-        elif pu_start >= len(self.cups) and pu_end >= cup_length:
-            pu_start = pu_start % cup_length
-            pu_end = pu_end % cup_length
-            pick_up = self.cups[pu_start: pu_end]
-            del (self.cups[pu_start: pu_end])
-        return pick_up
+    def find_destination_index(self, current, pick_up):
+        check = current - 1
+        while True:
+            if check in self.cups and check not in pick_up:
+                destination = check
+                break
+            if check < self.min:
+                check = self.max + 1
+            else:
+                check -= 1
+        return destination
 
     def get_order_string(self):
-        result = ''.join([str(x) for x in self.cups])
-        return result.split('1')[1] + result.split('1')[0]
+        result = ''
+        key = 1
+        for n in range(8):
+            result += str(self.cups[key])
+            key = self.cups[key]
+        return result
+
+    def get_two_labels_clockwise_of_one(self):
+        return self.cups[1], self.cups[self.cups[1]]
 
 
 def day23_part1(data_input, rounds):
-    game = Cups(data_input)
+    game = Cups(data_input, len(data_input))
     game.run_game(rounds)
     result = game.get_order_string()
     logging.info('RESULT: The final order of cups after %d rounds of Cup is: %s', game.round, result)
     return result
 
 
-def day23_part2(data_input):
-    game = Cups(data_input)
-    game.run_game(100)
-    # logging.info('RESULT: The winning score after %d rounds of Combat is: %d', game.rounds, winning_score)
-    return game
+def day23_part2(data_input, rounds):
+    game = Cups(data_input, 1000000)
+    game.run_game(rounds)
+    result_1, result_2 = game.get_two_labels_clockwise_of_one()
+    logging.info('RESULT: After %d rounds of Cup, the two cups clockwise of \'1\' are: %d & %d (Product: %d)',
+                 game.round, result_1, result_2, result_1 * result_2)
+    return result_1 * result_2
 
 
 # Input Files
@@ -106,19 +109,19 @@ else:
     logging.info("PASS: Part 1 Test PASSED")
 
 # Real Input - Part 1
-if day23_part1(main_input, 100) != 32162:
+if day23_part1(main_input, 100) != '38925764':
     raise Exception("FAIL: Part 1 FAILED!")
 else:
     logging.info("PASS: Part 1 PASSED")
 
 # Test Input - Part 2
-#if day23_part2(test_input) != 291:
-#    raise Exception("FAIL: Part 2 Test FAILED!")
-#else:
-#    logging.info("PASS: Part 2 Test PASSED")
+if day23_part2(test_input, 10000000) != 149245887792:
+    raise Exception("FAIL: Part 2 Test FAILED!")
+else:
+    logging.info("PASS: Part 2 Test PASSED")
 
 # Real Input - Part 2
-#if day23_part2(main_input) != 32534:
-#    raise Exception("FAIL: Part 2 FAILED!")
-#else:
-#    logging.info("PASS: Part 2 PASSED")
+if day23_part2(main_input, 10000000) != 131152940564:
+    raise Exception("FAIL: Part 2 FAILED!")
+else:
+    logging.info("PASS: Part 2 PASSED")
